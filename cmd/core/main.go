68ff72e62f896da1e2a4f1ea58956abcc17060ef
liuzhen21 <91461866+liuzhen21@users.noreply.github.com>
2021-11-17 19:13:18 +0800

    Feat/api (#56)
    * feat: add api
    
    * improvement: update api
    
    * improvement: handle conficts

---
+++ cmd/core/main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"os"
+	"os/signal"
+	"syscall"
+
+	Core_v1 "github.com/tkeel-io/core/api/core/v1"
+	openapi "github.com/tkeel-io/core/api/openapi/v1"
+	"github.com/tkeel-io/core/pkg/entities"
+	"github.com/tkeel-io/core/pkg/server"
+	"github.com/tkeel-io/core/pkg/service"
+
+	"github.com/panjf2000/ants/v2"
+	"github.com/tkeel-io/kit/app"
+	"github.com/tkeel-io/kit/log"
+	"github.com/tkeel-io/kit/transport"
+)
+
+var (
+	Name     string
+	HTTPAddr string
+	GRPCAddr string
+)
+
+func init() {
+	flag.StringVar(&Name, "name", "core", "app name.")
+	flag.StringVar(&HTTPAddr, "http_addr", ":31234", "http listen address.")
+	flag.StringVar(&GRPCAddr, "grpc_addr", ":31233", "grpc listen address.")
+}
+
+func main() {
+	flag.Parse()
+
+	httpSrv := server.NewHTTPServer(HTTPAddr)
+	grpcSrv := server.NewGRPCServer(GRPCAddr)
+	serverList := []transport.Server{httpSrv, grpcSrv}
+
+	coreApp := app.New(Name,
+		&log.Conf{
+			App:   Name,
+			Level: "debug",
+			Dev:   true,
+		},
+		serverList...,
+	)
+
+	{ // User service
+		// create coroutine pool.
+		coroutinePool, err := ants.NewPool(100)
+		if nil != err {
+			log.Fatal(err)
+		}
+
+		entityManager, err := entities.NewEntityManager(context.Background(), coroutinePool)
+		if nil != err {
+			log.Fatal(err)
+		}
+		EntitySrv, err := service.NewEntityService(context.Background(), entityManager)
+		if nil != err {
+			log.Fatal(err)
+		}
+		Core_v1.RegisterEntityHTTPServer(httpSrv.Container, EntitySrv)
+		Core_v1.RegisterEntityServer(grpcSrv.GetServe(), EntitySrv)
+
+		SubscriptionSrv, err := service.NewSubscriptionService(context.Background(), entityManager)
+		if nil != err {
+			log.Fatal(err)
+		}
+		Core_v1.RegisterSubscriptionHTTPServer(httpSrv.Container, SubscriptionSrv)
+		Core_v1.RegisterSubscriptionServer(grpcSrv.GetServe(), SubscriptionSrv)
+
+		OpenapiSrv := service.NewOpenapiService()
+		openapi.RegisterOpenapiHTTPServer(httpSrv.Container, OpenapiSrv)
+		openapi.RegisterOpenapiServer(grpcSrv.GetServe(), OpenapiSrv)
+	}
+
+	if err := coreApp.Run(context.TODO()); err != nil {
+		panic(err)
+	}
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGTERM, os.Interrupt)
+	<-stop
+
+	if err := coreApp.Stop(context.TODO()); err != nil {
+		panic(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.16
 require (
 	github.com/antlr/antlr4/runtime/Go/antlr v0.0.0-20211026222012-6af4c774c47b
 	github.com/dapr/go-sdk v1.2.0
+	github.com/emicklei/go-restful v2.15.0+incompatible
 	github.com/fsnotify/fsnotify v1.5.1
 	github.com/golang/protobuf v1.5.2
 	github.com/google/uuid v1.3.0
@@ -15,14 +16,16 @@ require (
 	github.com/sirupsen/logrus v1.8.1
 	github.com/smartystreets/assertions v1.2.0
 	github.com/smartystreets/gunit v1.4.2
-	github.com/spf13/cobra v1.2.1
 	github.com/spf13/viper v1.9.0
 	github.com/stretchr/testify v1.7.0
+	github.com/tkeel-io/kit v0.0.0-20211116085759-58aba7bc2d5d
+	github.com/tkeel-io/tkeel-interface/openapi v0.0.0-20211116091219-a18c7cbf2bf7
 	github.com/urfave/negroni v1.0.0
 	go.uber.org/atomic v1.9.0
 	golang.org/x/net v0.0.0-20210805182204-aaa1db679c0d // indirect
 	golang.org/x/sys v0.0.0-20211025201205-69cdffdb9359 // indirect
 	golang.org/x/text v0.3.7 // indirect
-	google.golang.org/grpc v1.40.0
+	google.golang.org/genproto v0.0.0-20211104193956-4c6863e31247
+	google.golang.org/grpc v1.42.0
 	google.golang.org/protobuf v1.27.1
 )
