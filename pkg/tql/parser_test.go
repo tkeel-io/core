622a47c6f4ac83ce8c5fdf428d95fa2a95da6849
JesseJia <jessejia@yunify.com>
2021-11-21 13:23:56 +0800

    fix golint

--- pkg/tql/parser_test.go
+++ pkg/tql/parser_test.go
@@ -1,10 +1,8 @@
 package tql
 
 import (
-	"fmt"
 	"testing"
 )
-
 
 func TestParserAndComputing(t *testing.T) {
 	tql := `insert into entity3 select
@@ -12,35 +10,33 @@ func TestParserAndComputing(t *testing.T) {
 		entity.property2.name as property2,
 		entity1.property1 + entity.property3 as property3`
 
-	fmt.Println("parse tql: ", tql)
+	log.Info("parse tql: ", tql)
 	l := Parse(tql)
 	cfg := l.GetParseConfigs()
-	fmt.Println("========\n ", cfg)
+	log.Info("========\n ", cfg)
 
 	in := make(map[string][]byte)
 	in["entity1.property1"] = []byte("1")
 	in["entity.property2.name"] = []byte("2")
 	in["entity.property3"] = []byte("3")
-	fmt.Println("========\n in: ", in)
+	log.Info("========\n in: ", in)
 	out := l.GetComputeResults(in)
-	fmt.Println(" out: ", out)
+	log.Info(" out: ", out)
 
 	// out2
 	in["entity1.property1"] = []byte("4")
 	in["entity.property2.name"] = []byte("7")
 	in["entity.property3"] = []byte("6")
-	fmt.Println("========\n in: ", in)
+	log.Info("========\n in: ", in)
 	out2 := l.GetComputeResults(in)
-	fmt.Println(" out2: ", out2)
-	
+	log.Info(" out2: ", out2)
 }
 
 func TestParser(t *testing.T) {
-
 	tql := `insert into target_entity select *`
 
-	fmt.Println("parse tql: ", tql)
+	log.Info("parse tql: ", tql)
 	l := Parse(tql)
 	cfg := l.GetParseConfigs()
-	fmt.Println("========\n ", cfg)
+	log.Info("========\n ", cfg)
 }
