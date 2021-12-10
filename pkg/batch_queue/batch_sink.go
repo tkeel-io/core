cd42a2cc0356d37945e2a2d704e55587f6280faa
gdlxSong <xgdlguo@yunify.com>
2021-12-10 11:09:24 +0800

    feat: add license

--- pkg/batch_queue/batch_sink.go
+++ pkg/batch_queue/batch_sink.go
@@ -1,24 +1,17 @@
 /*
- * Copyright (C) 2019 Yunify, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this work except in compliance with the License.
- * You may obtain a copy of the License in the LICENSE file, or at:
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+Copyright 2021 The tKeel Authors.
 
-/**
-SinkBatch
-1. 线程池，限定最大并发数
-2. 累计 message, 定时提交
-3. 可以 Flush，手动触发提交
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 */
 
 package batchqueue
