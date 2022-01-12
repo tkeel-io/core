d6f5c022240e331169c865aac21c26a44cdb4b8a
uptutu <alexkung@outlook.com>
2022-01-12 16:10:57 +0800

    fix: testdata

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -8,6 +8,6 @@ import (
 
 func TestInitConfig(t *testing.T) {
 	assert.Equal(t, "", config.Server.AppID)
-	InitConfig("../../testdata/config.yml")
+	InitConfig("../../testdata/testconfig.yml")
 	assert.Equal(t, "core", config.Server.AppID)
 }
