82efda855701fc20b459954ef251983cdc7d30a2
uptutu <alexkung@outlook.com>
2022-01-18 16:02:07 +0800

    feat: add influxdb flux qeury

--- pkg/resource/timeseries/influx.go
+++ pkg/resource/timeseries/influx.go
@@ -3,22 +3,25 @@ package timeseries
 import (
 	"context"
 	"encoding/json"
+	"fmt"
+	"github.com/tkeel-io/core/pkg/resource"
+	"strings"
 
 	influxdb2 "github.com/influxdata/influxdb-client-go"
 	"github.com/influxdata/influxdb-client-go/api"
 	"github.com/pkg/errors"
-	"github.com/tkeel-io/core/pkg/resource"
 	"github.com/tkeel-io/kit/log"
 	"go.uber.org/zap"
 )
 
 var EngineInflux Engine = "influxdb"
 
-// Influx allows writing to InfluxDB.
+// Influx allows writing and reading InfluxDB.
 type Influx struct {
 	cfg      *InfluxConfig
 	client   influxdb2.Client
 	writeAPI api.WriteAPIBlocking
+	queryAPI api.QueryAPI
 }
 
 type InfluxConfig struct {
@@ -62,6 +65,7 @@ func (i *Influx) Init(metadata resource.TimeSeriesMetadata) error {
 	client := influxdb2.NewClient(i.cfg.URL, i.cfg.Token)
 	i.client = client
 	i.writeAPI = i.client.WriteAPIBlocking(i.cfg.Org, i.cfg.Bucket)
+	i.queryAPI = i.client.QueryAPI(i.cfg.Org)
 
 	return nil
 }
@@ -102,5 +106,147 @@ func (i *Influx) Write(ctx context.Context, req *WriteRequest) *Response {
 }
 
 func (i Influx) Query(ctx context.Context, req QueryRequest) *Response {
-	return &Response{}
+	res, err := i.queryAPI.QueryRaw(ctx, req.ToRawQuery(), influxdb2.DefaultDialect())
+	if err != nil {
+		return &Response{Error: err}
+	}
+	return &Response{Raw: []byte(res)}
+}
+
+type FluxOption func(raw string) string
+
+func FluxRangeOption(start, end string) FluxOption {
+	if end == "" {
+		end = "now()"
+	}
+	return func(raw string) string {
+		raw += fmt.Sprintf("start: %s, stop: %s", start, end)
+		return raw
+	}
+}
+
+func FluxMeasurementFilterOption(operator, value string) FluxOption {
+	return func(raw string) string {
+		if strings.Contains(raw, "==") &&
+			len(raw)-2 == strings.LastIndex(raw, "\"") {
+			raw = strings.TrimSuffix(raw, "\n") + " " + operator + "\n"
+		}
+		raw += "r._measurement == " + fmt.Sprintf("%q\n", value)
+		return raw
+	}
+}
+
+func FluxFiledFilterOption(operator, value string) FluxOption {
+	return func(raw string) string {
+		if strings.Contains(raw, "==") &&
+			len(raw)-2 == strings.LastIndex(raw, "\"") {
+			raw = strings.TrimSuffix(raw, "\n") + " " + operator + "\n"
+		}
+		raw += "r._field == " + fmt.Sprintf("%q\n", value)
+		return raw
+	}
+}
+
+func FluxTagFilterOption(operator, value string) FluxOption {
+	return func(raw string) string {
+		if strings.Contains(raw, "==") &&
+			len(raw)-2 == strings.LastIndex(raw, "\"") {
+			raw = strings.TrimSuffix(raw, "\n") + " " + operator + "\n"
+		}
+		raw += "r.tag == " + fmt.Sprintf("%q\n", value)
+		return raw
+	}
+}
+
+func FluxGroupQueryOption(columns []string, mode ...string) FluxOption {
+	for i := 0; i < len(columns); i++ {
+		columns[i] = fmt.Sprintf("%q", columns[i])
+	}
+	return func(raw string) string {
+		raw += fmt.Sprintf("columns:[%s]", strings.Join(columns, ","))
+		if len(mode) != 0 {
+			raw += fmt.Sprintf(", mode:%q", mode[0])
+		}
+		return raw
+	}
+}
+
+func FluxSortOption(columns []string) FluxOption {
+	for i := 0; i < len(columns); i++ {
+		columns[i] = fmt.Sprintf("%q", columns[i])
+	}
+	return func(raw string) string {
+		raw += fmt.Sprintf("columns: [%s]", strings.Join(columns, ","))
+		return raw
+	}
+}
+
+func FluxLimitOption(n int) FluxOption {
+	return func(raw string) string {
+		raw += fmt.Sprintf("n: %d", n)
+		return raw
+	}
+}
+
+type FluxQuery struct {
+	FromBucket string
+	RangeOpt   FluxOption
+	FilterOpts []FluxOption
+	GroupOpt   FluxOption
+	SortOpt    FluxOption
+	LimitOpt   FluxOption
+	raw        string
+}
+
+func (q *FluxQuery) ToRawQuery() string {
+	if q.raw == "" {
+		q.raw = fmt.Sprintf("from(bucket: %q)\n", q.FromBucket)
+	}
+	raw := q.raw
+
+	// add Range.
+	if q.RangeOpt != nil {
+		rangeStart := "|> range("
+		rangeEnd := ")\n"
+		raw += rangeStart
+		raw = q.RangeOpt(raw) + rangeEnd
+	}
+
+	// add Filter.
+	if len(q.FilterOpts) != 0 {
+		filterStart := "|> filter(fn: (r) =>\n"
+		filterEnd := ")\n"
+		raw += filterStart
+		for i := 0; i < len(q.FilterOpts); i++ {
+			raw = q.FilterOpts[i](raw)
+		}
+		raw += filterEnd
+	}
+
+	// add Group.
+	if q.GroupOpt != nil {
+		groupStart := "|> group("
+		groupEnd := ")\n"
+		raw += groupStart
+		raw = q.GroupOpt(raw) + groupEnd
+	}
+
+	// add Sort.
+	if q.SortOpt != nil {
+		sortStart := "|> sort("
+		sortEnd := ")\n"
+		raw += sortStart
+		raw = q.SortOpt(raw) + sortEnd
+	}
+
+	// add Limit.
+	if q.LimitOpt != nil {
+		limitStart := "|> limit("
+		limitEnd := ")"
+		raw += limitStart
+		raw = q.LimitOpt(raw) + limitEnd
+	}
+
+	q.raw = strings.TrimSuffix(raw, "\n")
+	return q.raw
 }
--- pkg/resource/timeseries/influx_test.go
+++ pkg/resource/timeseries/influx_test.go
@@ -1,6 +1,7 @@
 package timeseries
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
@@ -26,4 +27,58 @@ func Test_Write(t *testing.T) {
 	// 		t.Log("write influx failed", err)
 	// 	}
 	// }
+}
+
+func TestFluxQuery(t *testing.T) {
+	tests := []struct {
+		name string
+		q    FluxQuery
+		want string
+	}{
+		{"bucket", FluxQuery{FromBucket: "test"}, `from(bucket: "test")`},
+		{"bucket and range", FluxQuery{FromBucket: "test", RangeOpt: FluxRangeOption("now()", "now()")},
+			`from(bucket: "test")
+|> range(start: now(), stop: now())`},
+		{"bucket and range and filter", FluxQuery{FromBucket: "test", RangeOpt: FluxRangeOption("now()", "now()"), FilterOpts: []FluxOption{FluxMeasurementFilterOption("and", "_test")}},
+			`from(bucket: "test")
+|> range(start: now(), stop: now())
+|> filter(fn: (r) =>
+r._measurement == "_test"
+)`},
+		{"bucket and range and multiple filter",
+			FluxQuery{FromBucket: "test", RangeOpt: FluxRangeOption("now()", "now()"),
+				FilterOpts: []FluxOption{FluxMeasurementFilterOption("and", "_test"), FluxFiledFilterOption("and", "_field"), FluxTagFilterOption("or", "myTag")}},
+			`from(bucket: "test")
+|> range(start: now(), stop: now())
+|> filter(fn: (r) =>
+r._measurement == "_test" and
+r._field == "_field" or
+r.tag == "myTag"
+)`},
+		{"bucket and range and multiple filter and sort",
+			FluxQuery{FromBucket: "test", RangeOpt: FluxRangeOption("now()", "now()"),
+				FilterOpts: []FluxOption{FluxMeasurementFilterOption("and", "_test"), FluxFiledFilterOption("and", "_field")},
+				SortOpt:    FluxSortOption([]string{"myColumn1", "myColumn2"})},
+			`from(bucket: "test")
+|> range(start: now(), stop: now())
+|> filter(fn: (r) =>
+r._measurement == "_test" and
+r._field == "_field"
+)
+|> sort(columns: ["myColumn1","myColumn2"])`},
+		{"bucket and group with mode by", FluxQuery{FromBucket: "test", GroupOpt: FluxGroupQueryOption([]string{"column1", "column2"}, "by")},
+			`from(bucket: "test")
+|> group(columns:["column1","column2"], mode:"by")`},
+		{"bucket and group without mode", FluxQuery{FromBucket: "test", GroupOpt: FluxGroupQueryOption([]string{"column1", "column2"})},
+			`from(bucket: "test")
+|> group(columns:["column1","column2"])`},
+		{"bucket and group without mode and limit", FluxQuery{FromBucket: "test", GroupOpt: FluxGroupQueryOption([]string{"column1", "column2"}), LimitOpt: FluxLimitOption(5)},
+			`from(bucket: "test")
+|> group(columns:["column1","column2"])
+|> limit(n: 5)`},
+	}
+
+	for _, test := range tests {
+		assert.Equal(t, test.want, test.q.ToRawQuery())
+	}
 }
