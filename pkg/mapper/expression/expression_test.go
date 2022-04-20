327075ed4d7057c7b161a75fc40b1a1b47bbf58a
gdlxSong <xgdlguo@yunify.com>
2022-04-20 11:16:58 +0800

    feat: improvement.

--- pkg/mapper/expression/expression_test.go
+++ pkg/mapper/expression/expression_test.go
@@ -9,10 +9,11 @@ import (
 )
 
 func Test_Expression(t *testing.T) {
-	exprIns, err := NewExpr("a.device123", nil)
+	exprIns, err := NewExpr(" device234.properties.sysField._spacePath + '/device123' ", nil)
 	assert.Nil(t, err)
 	t.Log(exprIns.Entities())
-	res, err := exprIns.Eval(context.Background(), map[string]tdtl.Node{"a.device123": tdtl.NewInt64(20)})
+	res, err := exprIns.Eval(context.Background(),
+		map[string]tdtl.Node{"device234.properties.sysField._spacePath": tdtl.NewString("tomas")})
 	assert.Nil(t, err)
 	t.Log(res.String())
 }
